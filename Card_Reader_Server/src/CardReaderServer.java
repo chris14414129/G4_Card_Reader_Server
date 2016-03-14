c68da0b4e67c607f838ea7ed303fe34e57f109ac
“chris” <chris@email>
2016-03-14 11:03:23 +0000

    added uni login details

--- Card_Reader_Server/src/CardReaderServer.java
+++ Card_Reader_Server/src/CardReaderServer.java
@@ -4,12 +4,18 @@ public class CardReaderServer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//connection to server/table 
-				String url= "jdbc:mysql://192.168.56.3:3306/Plan_A";
-				String userName= "student";
-				String password= "student";
+		//connection to server/table via vagrant
+				String urlVagrant= "jdbc:mysql://192.168.56.3:3306/Plan_A";
+				String userNameVagrant= "student";
+				String passwordVagrant= "student";
 		
-				
+		//connection to server/table via uni DB
+		String url= "jdbc:mysql://194.81.104.22:3306/db_group4_1516";
+		String userName= "group4_1516";
+		String password= "group4";
+		
+		
+		
 		
 	}
 
